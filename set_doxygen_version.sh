8641d0787803239b1ef292085790dc4425e346ac
fgazzah <fgazzah@fredj.gazzah>
2018-06-28 21:22:44 +0200

    update set_version_oxygen script

--- set_doxygen_version.sh
+++ set_doxygen_version.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-DOXYGENE_FILE=$1
-VERSION_HEADER_FILE=$2
-#TODO: verif both files exists
+# this script set the doxyfile Version_Number with the defined version in project version header file
+
+usage()
+{
+        echo "Usage : $0 Version_header_file"
+        echo ""
+}
+
+DOXYGENE_FILE=Doxyfile
+if [ ! -e $DOXYGENE_FILE ] ; then
+        echo "Warn: file '$DOXYGENE_FILE' not found in `pwd` ";
+        exit 0;
+fi
+
+if [ -n "$1" ]; then
+        VERSION_HEADER_FILE=$1
+	if [ ! -f $VERSION_HEADER_FILE ] ; then
+		echo "Warn: file '$VERSION_HEADER_FILE' not found";
+	       	exit 0;
+        fi
+else
+	echo 'Warn: missed version_header_file argument'
+	usage
+	exit 0
+fi
 
 MAJOR_VERSION=$(cat $VERSION_HEADER_FILE | awk 'match($0, /_VERSION_MAJOR[ \t]+[0-9]+/) { print substr($0, RSTART+15, RLENGTH-15)}'|xargs)
 MINOR_VERSION=$(cat $VERSION_HEADER_FILE | awk 'match($0, /_VERSION_MINOR[ \t]+[0-9]+/) { print substr($0, RSTART+15, RLENGTH-15)}'|xargs)
@@ -11,3 +33,4 @@ PATCH_VERSION=$(cat $VERSION_HEADER_FILE | awk 'match($0, /_VERSION_PATCH[ \t]+[
 VERSION_NUMBER=$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION 
 
 sed -i  "s/PROJECT_NUMBER[ \t]*=/PROJECT_NUMBER=\t$VERSION_NUMBER/" $DOXYGENE_FILE
+
